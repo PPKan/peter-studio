ffaa17dd187c5881ce4e433de21c44d263a86de8
Peter Work Laptop <petervsjim@gmail.com>
2022-07-01 17:30:17 +0800

    finish about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import "./App.scss";
+import bigTree from "./images/big-tree.svg";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setLoader(false);
+  });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+      <main className="about">
+        <Navbar />
+        <img
+          className="about__tree"
+          src={bigTree}
+          alt="big tree for entrance"
+        />
+        <div className="about__text">
+          <p className="about__text--title">Art, Language, Technology</p>
+          <p className="about__text--description">
+            The bridge between human and digits.
+          </p>
+        </div>
+      </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import navIcon from "../../images/nav-icon.svg";
+import "./Navbar.scss"
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  return (
+    <nav className="nav">
+      <img className="nav__icon" src={navIcon} alt="icon for navbar" />
+      <li className="nav__list">
+        <Link to="about">About</Link>
+        <Link to="solutions">Solutions</Link>
+        <Link to="works">Works</Link>
+        <Link to="contacts">Contacts</Link>
+      </li>
+    </nav>
+  );
+}
